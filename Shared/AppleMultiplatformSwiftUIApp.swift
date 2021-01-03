b8044b40e373b283de2e5acc4f942baa6214ccf9
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-01-03 15:39:55 +0300

    Initial Commit

---
+++ Shared/AppleMultiplatformSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMultiplatformSwiftUIApp.swift
+//  Shared
+//
+//  Created by Ali Kadir BAGCIOGLU on 3.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMultiplatformSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
